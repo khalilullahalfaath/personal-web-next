ee81f53d4b09892c44fc6e6e0dbf6edd3ac14718
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2024-04-25 21:32:21 +0800

    feat: create simple hero section components

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">Hello, I'm Alil</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
